20d02409e6eab508167a45cf7be324055d58500d
haassanbash <hassanbasheer135@gmail.com>
2024-09-10 15:37:14 +0300

    create Header Part-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { ColorModeContext, useMode } from "./theme";
 import Header1 from "./components/Header/Header1";
 import Header2 from "./components/Header/Header2";
 import Header3 from "./components/Header/Header3";
+import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
 
@@ -12,19 +12,15 @@ function App() {
 
   return (
     <>
-
       <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-      <CssBaseline />
-
-          <Header1 /> 
-      <Header2 /> 
-      <Header3 /> 
-      </ThemeProvider>
-     
-    </ColorModeContext.Provider>
-
-     </>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Header1 />
+          <Header2 />
+          <Header3 />
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </>
   )
 }
 
--- src/components/Header/Header1.jsx
+++ src/components/Header/Header1.jsx
@@ -1,37 +1,92 @@
-import { Box, IconButton, Stack, Typography } from "@mui/material";
-export default function Header1(){
-    return(
+import { useContext } from "react";
+import { ColorModeContext } from "../../theme";
+import { Box, IconButton, Stack, Typography, useTheme } from "@mui/material";
+import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+export default function Header1() {
+    const colorMode = useContext(ColorModeContext);
+    const theme = useTheme();
+    return (
         <>
-        <Box
-        sx={{
-            bgcolor:"#2B3445"
-        }}>
-            <Stack direction={"row"} alignItems={"center"}>
-            <Typography 
-            sx={{
-                mr:2,
-                padding:"3px 10px",
-                bgcolor:"#D23f57",
-                borderRadius:'12px',
-                fontSize:"10px",
-                fontWeight:"bold",
-                color:'#fff'
-            }}>
+            <Box
+                sx={{
+                    bgcolor: "#2B3445"
+                }}>
+                <Stack direction={"row"} alignItems={"center"}>
+                    <Typography
+                        sx={{
+                            mr: 2,
+                            padding: "3px 10px",
+                            bgcolor: "#D23f57",
+                            borderRadius: '12px',
+                            fontSize: "10px",
+                            fontWeight: "bold",
+                            color: '#fff'
+                        }}>
                         HOT
-            </Typography>
-             <Typography
-             sx={{
-       
-                fontSize:"12px",
-                fontWeight:"300",
-                color:'#fff'
-            }}
-            variant="body2">
-                Free Express Shipping
-            </Typography>
-      
-            </Stack>
-        </Box>
+                    </Typography>
+                    <Typography
+                        sx={{
+
+                            fontSize: "12px",
+                            fontWeight: "300",
+                            color: '#fff'
+                        }}
+                        variant="body2">
+                        Free Express Shipping
+                    </Typography>
+                    <Box flexGrow={1} />
+
+                <div>
+                    {theme.palette.mode === "light" ? (
+                        <IconButton
+                            onClick={() => {
+                                localStorage.setItem(
+                                    "mode",
+                                    theme.palette.mode === "dark" ? "light" : "dark"
+                                );
+                                colorMode.toggleColorMode();
+                            }}
+                            color="inherit"
+                        >
+                            <LightModeOutlined fontSize="small" />
+                        </IconButton>
+                    ) : (
+                        <IconButton
+                            onClick={() => {
+                                localStorage.setItem(
+                                    "mode",
+                                    theme.palette.mode === "dark" ? "light" : "dark"
+                                );
+                                colorMode.toggleColorMode();
+                            }}
+                            color="inherit"
+                        >
+                            <DarkModeOutlined  fontSize="small" />
+                        </IconButton>
+                    )}
+                </div>
+                <InstagramIcon 
+                sx={{
+                    fontSize:"14px",
+                    color:"fff"
+                }}/>
+                <XIcon
+                sx={{
+                    fontSize:"14px",
+                    color:"fff",
+                    mx:1
+                }} />
+                <FacebookIcon
+                sx={{
+                    fontSize:"14px",
+                    color:"fff"
+                }} />
+                </Stack>
+            </Box>
         </>
     )
 }
